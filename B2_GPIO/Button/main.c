a56a94e9a35346e3448cac137ed08ec5503dd04e
PhamLam21 <lamchi920@gmail.com>
2025-02-03 14:23:18 +0700

    Gpio

---
+++ B2_GPIO/Button/main.c
@@ -0,0 +1,48 @@
+/*
+	Author: Pham Tung Lam
+	Date: 3/2/2025
+	Description: Thay doi den bang button
+*/
+
+#include "stm32f10x.h"                  // Device header
+#include "stm32f10x_rcc.h"              // Keil::Device:StdPeriph Drivers:RCC
+#include "stm32f10x_gpio.h"             // Keil::Device:StdPeriph Drivers:GPIO
+
+void delay(uint32_t time) {
+	for(uint32_t i = 0; i < time; i++) {}
+}
+
+void setUp() {
+	//Cap clock cho GPIOC
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	
+	//Cau hinh GPIOC
+	GPIO_InitTypeDef GPIO_InitStruct;
+	
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOC, &GPIO_InitStruct);
+	
+	//Cau hinh GPIOA
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPD;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+int main() {
+	setUp();
+	while(1) {
+		if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)) {
+			while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0));
+			if(GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_13)) {
+				GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+			} else {
+				GPIO_SetBits(GPIOC, GPIO_Pin_13);
+			}
+		}
+	}
+}
